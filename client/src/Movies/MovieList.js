a3b51e9860af5152796cf461add4c238b80fd239
Adam Mathieson <mathieson1222@gmail.com>
2019-02-25 15:42:46 -0800

    refactored MovieCard.js as component for MovieList and Movie

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 import { Link } from "react-router-dom";
 
+import MovieCard from "./MovieCard";
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -37,22 +39,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars, id } = movie;
   return (
     <Link to={`/movies/${id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard movie={movie} />
     </Link>
   );
 }
