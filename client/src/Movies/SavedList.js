f6c5d079c2fc3fccb9e449189c28edc207155d10
Adam Mathieson <mathieson1222@gmail.com>
2019-02-26 15:51:53 -0800

    added function to savelist to move between saved movies

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+
 const SavedList = props => {
     return (
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {props.list.map(movie => (
-          <span key={movie.id} className="saved-movie">{movie.title} </span>
+          <span key={movie.id} className="saved-movie" onClick={goMovie}>{movie.title}</span>
         ))}
           <div className="home-button" onClick={goHome}>Home</div>
       </div>
     );
+
+    function goMovie() {
+      props.history.push("/movies/:id")
+    }
 
     function goHome() {
       props.history.push("/");
